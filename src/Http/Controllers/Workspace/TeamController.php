060733cea028735c69a7b43c9de9d8310edb2328
timsinakiran <timsinakiran@gmail.com>
2026-06-13 09:48:08 +0545

    test(pm): cover team update-deny; inject Request in TeamController@destroy

--- src/Http/Controllers/Workspace/TeamController.php
+++ src/Http/Controllers/Workspace/TeamController.php
@@ -3,6 +3,7 @@
 namespace RayzenAI\ProjectManagement\Http\Controllers\Workspace;
 
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -47,9 +48,9 @@ class TeamController extends Controller
         return back()->with('workspace_flash', ['success' => true, 'message' => 'Team updated.']);
     }
 
-    public function destroy(Team $team): RedirectResponse
+    public function destroy(Request $request, Team $team): RedirectResponse
     {
-        abort_unless(WorkspaceAccess::isSuperAdmin(request()->user()), 403);
+        abort_unless(WorkspaceAccess::isSuperAdmin($request->user()), 403);
 
         $team->delete();
 
