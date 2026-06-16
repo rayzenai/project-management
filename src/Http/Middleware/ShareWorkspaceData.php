215a12b994e5538227fa6a7dae8daa5f00114e1c
timsinakiran <timsinakiran@gmail.com>
2026-06-16 19:57:23 +0545

    feat: email_notifications preference (stored; delivery deferred)

--- src/Http/Middleware/ShareWorkspaceData.php
+++ src/Http/Middleware/ShareWorkspaceData.php
@@ -80,6 +80,7 @@ class ShareWorkspaceData
                 'theme' => $theme,
                 'mode' => $themes[$theme]['mode'] ?? null,
                 'tokens' => app(ResolveThemeTokens::class)->resolved($theme, $fontOverride),
+                'email_notifications' => $request->user()?->appearance()['email_notifications'] ?? true,
             ];
         });
 
